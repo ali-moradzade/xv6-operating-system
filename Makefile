213301faa88acb3c7cd691167b17e5768d0de684
keivanipchihagh <keivanipchihagh@gmail.com>
2022-01-26 01:04:17 +0330

    Updated sanity checks

--- Makefile
+++ Makefile
@@ -192,6 +192,8 @@ UPROGS=\
 	_getReadCountTest\
 	_threads\
 	_sanity\
+	_test_roundrobin\
+	_test_priority\
 
 fs.img: mkfs README $(UPROGS)
 	./mkfs fs.img README $(UPROGS)
@@ -259,7 +261,7 @@ qemu-nox-gdb: fs.img xv6.img .gdbinit
 # check in that version.
 
 EXTRA=\
-	mkfs.c ulib.c user.h cat.c echo.c forktest.c grep.c kill.c sanity.c\
+	mkfs.c ulib.c user.h cat.c echo.c forktest.c grep.c kill.c sanity.c test_roundrobin.c, test_priority.c\
 	ln.c ls.c mkdir.c rm.c stressfs.c usertests.c wc.c zombie.c\
 	printf.c umalloc.c\
 	README dot-bochsrc *.pl toc.* runoff runoff1 runoff.list\
---
+++ test_priority.c
@@ -0,0 +1,63 @@
+#include "types.h"
+#include "user.h"
+
+#define PROC_COUNT 10       // number of processes
+#define PRINT_COUNT 10      // number of prints for each process
+#define GROUP_COUNT 5       // number of processes in each group
+
+int main(int argc, char *argv[]) {
+
+	printf(1, "Running sanity check for Priority...\n");
+    sleep(100);
+
+	int i, j;		                                                                // temp variables
+	int pid, waiting_time, running_time, sleeping_time, times[PROC_COUNT][3];		// processes variables
+
+    for (i = 0; i < PROC_COUNT; i++)
+		for (j = 0; j < 3; j++)
+			times[i][j] = 0;
+
+	// create child processes and assign tasks to them
+	for (i = 0; i < PROC_COUNT; i++) {
+		pid = fork();
+
+        #if defined(PRIORITY)
+            set_prio((PROC_COUNT - i) / GROUP_COUNT + 1); break;
+        #endif
+		
+		// use only chile processes, consume CPU time and exit
+		if (pid == 0) {
+            for (int j = 0; j < PRINT_COUNT; j++)
+			    printf(1, "PID\t%d : %d\n", getpid(), j);
+			exit();
+		}
+		continue;	// keep father busy spawning children
+	}
+
+	// print timing properties for each process
+    sleep(100);
+    printf(1, "\n+ Individual Times\n");
+	for (i = 0; i < PROC_COUNT; i++) {
+
+        // wait for the process to exit, then return properties
+		pid = wait2(&waiting_time, &running_time, &sleeping_time);
+        
+        times[i][0] += sleeping_time;       // sleeping time
+        times[i][1] += waiting_time;        // waiting time
+        times[i][2] += running_time;        // running time
+
+        printf(1, "- PID: %d),\tready: %d,\trunning: %d,\tsleeping: %d,\tturnaround: %d\n", pid, waiting_time, running_time, sleeping_time, waiting_time + running_time + sleeping_time);        
+	}
+
+    // Calculate averages
+    int avg_times[3];
+    for (j = 0; j < 3; j++) {
+	    for (i = 0; i < PROC_COUNT; i++)
+            avg_times[j] += times[i][j];
+
+        avg_times[j] /= PROC_COUNT;      // average
+    }
+
+    printf(1, "\n+ Average Times\n- Waiting Time: %d,\tRunning Time: %d,\tSleeping Time: %d,\tTurnaround Time: %d\n", avg_times[1], avg_times[2], avg_times[0], avg_times[1] + avg_times[2]);
+	exit();
+}
