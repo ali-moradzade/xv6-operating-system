a358041929a08022024f34171c711db3e6ee5018
Ali Moradzade <alimorizz1379@gmail.com>
2022-01-25 20:05:00 +0330

    adding cls to clear screen, changing some numbers based on project description

--- param.h
+++ param.h
@@ -11,5 +11,5 @@
 #define LOGSIZE      (MAXOPBLOCKS*3)  // max data blocks in on-disk log
 #define NBUF         (MAXOPBLOCKS*3)  // size of disk block cache
 #define FSSIZE     1000  // size of file system in blocks
-#define QUANTA        5  // process preemption will be done every quanta size (measured in clock ticks)
+#define QUANTA       10  // process preemption will be done every quanta size (measured in clock ticks)
 
